e217552c6f5497957471840bca054b5de8a91c32
Andy Vandenberghe <sajaki9@gmail.com>
2011-04-23 11:49:03 +0200

    light refactoring

--- root/includes/bbdkp/raidplanner/raidplanner_base.php
+++ root/includes/bbdkp/raidplanner/raidplanner_base.php
@@ -45,7 +45,89 @@ class raidplanner_base
 	public $mode_sel_code = "";
 	
 	
+	/* initialize global variables used throughout
+	   all of the calendar functions
+	*/
+	public function _init_calendar_data()
+	{
+		global $auth, $db, $user, $config; 
+		
+		/* check to see if we have already initialized things */
+		if( count($this->month_names) == 0 )
+		{
+			$this->month_names[1] = "January";
+			$this->month_names[2] = "February";
+			$this->month_names[3] = "March";
+			$this->month_names[4] = "April";
+			$this->month_names[5] = "May";
+			$this->month_names[6] = "June";
+			$this->month_names[7] = "July";
+			$this->month_names[8] = "August";
+			$this->month_names[9] = "September";
+			$this->month_names[10] = "October";
+			$this->month_names[11] = "November";
+			$this->month_names[12] = "December";
 	
+			//find the available events from bbDKP, store them in a global array
+			$sql = 'SELECT * FROM ' . EVENTS_TABLE . ' ORDER BY event_id';
+			$result = $db->sql_query($sql);
+			$this->raid_plan_count = 0;
+			while ($row = $db->sql_fetchrow($result))
+			{
+				$this->raid_plan_ids[$this->raid_plan_count] = $row['event_id'];
+				$this->raid_plan_names[$this->raid_plan_count] = $row['event_name'];
+				$this->raid_plan_colors[$row['event_id']] = $row['event_color'];
+				$this->raid_plan_images[$row['event_id']] = $row['event_imagename'];
+				$this->raid_plan_displaynames[$row['event_id']] = $row['event_name'];
+				$this->raid_plan_count++;
+			}
+			$db->sql_freeresult($result);
+		}
+	
+		// always refresh the date...
+	
+		//get the current date and set it into an array
+		$this->date['day'] = request_var('calD', '');
+		$this->date['month'] = request_var('calM', '');
+		$this->date['month_no'] = request_var('calM', '');
+		$this->date['year'] = request_var('calY', '');
+	
+		$temp_now_time = time() + $user->timezone + $user->dst;
+	
+		if( $this->date['day'] == "" )
+		{
+			$this->date['day'] = gmdate("d", $temp_now_time);
+		}
+	
+		if( $this->date['month'] == "" )
+		{
+			$this->date['month'] = gmdate("F", $temp_now_time);
+			$this->date['month_no'] = gmdate("n", $temp_now_time);
+			$this->date['prev_month'] = gmdate("n", $temp_now_time) - 1;
+			$this->date['next_month'] = gmdate("n", $temp_now_time) + 1;
+	
+		}
+		else
+		{
+			$this->date['month'] = $this->month_names[$this->date['month']];
+			$this->date['prev_month'] = $this->date['month'] - 1;
+			$this->date['next_month'] = $this->date['month'] + 1;
+		}
+	
+		if( $this->date['year'] == "" )
+		{
+			$this->date['year']	= gmdate('Y', $temp_now_time);
+		}
+		
+		// make sure this day exists - ie there is no February 31st.
+		$number_days = gmdate("t", gmmktime( 0,0,0,$this->date['month_no'], 1, $this->date['year']));
+		if( $number_days < $this->date['day'] )
+		{
+		    $this->date['day'] = $number_days;
+		}
+	}
+	
+
 	/*
 	 * checks if a user has right to post a new raid
 	 * @param byref : $raidplan_data
@@ -190,90 +272,6 @@ class raidplanner_base
 		}
 		
 		return $raidplan_data;
-	}
-	
-	
-	
-	/* initialize global variables used throughout
-	   all of the calendar functions
-	*/
-	public function _init_calendar_data()
-	{
-		global $auth, $db, $user, $config; 
-		
-		/* check to see if we have already initialized things */
-		if( count($this->month_names) == 0 )
-		{
-			$this->month_names[1] = "January";
-			$this->month_names[2] = "February";
-			$this->month_names[3] = "March";
-			$this->month_names[4] = "April";
-			$this->month_names[5] = "May";
-			$this->month_names[6] = "June";
-			$this->month_names[7] = "July";
-			$this->month_names[8] = "August";
-			$this->month_names[9] = "September";
-			$this->month_names[10] = "October";
-			$this->month_names[11] = "November";
-			$this->month_names[12] = "December";
-	
-			//find the available raidplan types:
-			$sql = 'SELECT * FROM ' . EVENTS_TABLE . ' ORDER BY raidplan_id';
-			$result = $db->sql_query($sql);
-			$this->raid_plan_count = 0;
-			while ($row = $db->sql_fetchrow($result))
-			{
-				$this->raid_plan_ids[$this->raid_plan_count] = $row['raidplan_id'];
-				$this->raid_plan_names[$this->raid_plan_count] = $row['raidplan_name'];
-				$this->raid_plan_colors[$row['raidplan_id']] = $row['raidplan_color'];
-				$this->raid_plan_images[$row['raidplan_id']] = $row['raidplan_imagename'];
-				$this->raid_plan_displaynames[$row['raidplan_id']] = $row['raidplan_name'];
-				$this->raid_plan_count++;
-			}
-			$db->sql_freeresult($result);
-		}
-	
-		// always refresh the date...
-	
-		//get the current date and set it into an array
-		$this->date['day'] = request_var('calD', '');
-		$this->date['month'] = request_var('calM', '');
-		$this->date['month_no'] = request_var('calM', '');
-		$this->date['year'] = request_var('calY', '');
-	
-		$temp_now_time = time() + $user->timezone + $user->dst;
-	
-		if( $this->date['day'] == "" )
-		{
-			$this->date['day'] = gmdate("d", $temp_now_time);
-		}
-	
-		if( $this->date['month'] == "" )
-		{
-			$this->date['month'] = gmdate("F", $temp_now_time);
-			$this->date['month_no'] = gmdate("n", $temp_now_time);
-			$this->date['prev_month'] = gmdate("n", $temp_now_time) - 1;
-			$this->date['next_month'] = gmdate("n", $temp_now_time) + 1;
-	
-		}
-		else
-		{
-			$this->date['month'] = $this->month_names[$this->date['month']];
-			$this->date['prev_month'] = $this->date['month'] - 1;
-			$this->date['next_month'] = $this->date['month'] + 1;
-		}
-	
-		if( $this->date['year'] == "" )
-		{
-			$this->date['year']	= gmdate('Y', $temp_now_time);
-		}
-		
-		// make sure this day exists - ie there is no February 31st.
-		$number_days = gmdate("t", gmmktime( 0,0,0,$this->date['month_no'], 1, $this->date['year']));
-		if( $number_days < $this->date['day'] )
-		{
-		    $this->date['day'] = $number_days;
-		}
 	}
 	
 	/*------------------------------------------------------
