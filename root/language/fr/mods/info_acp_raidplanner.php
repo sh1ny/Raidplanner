4ce37d61528d2fc378e3190f0eae1f6ac3d4b5fb
Andy Vandenberghe <sajaki9@gmail.com>
2011-11-03 20:57:56 +0100

    french-german support

---
+++ root/language/fr/mods/info_acp_raidplanner.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * bbdkp acp language file for raidplanner module
+ * 
+ * @package bbDkp
+ * @copyright 2010 bbdkp <http://code.google.com/p/bbdkp/>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @version $Id$
+ * 
+ */
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+// Create the lang array if it does not already exist
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// Merge the following language entries into the lang array
+$lang = array_merge($lang, array(
+	'ACP_CAT_RAIDPLANNER' 				=> 'Raidplanner', //main tab 
+	'ACP_RAIDPLANNER' 					=> 'Raidplanner', //category
+	'ACP_RAIDPLANNER_SETTINGS'  		=> 'Raidplanner Settings', 	//module
+	'ACP_RAIDPLANNER_SETTINGS_EXPLAIN' 	=> 'Here you can configure Raid Planner settings',
+	'ACP_RAIDPLANNER_EVENTSETTINGS'		=> 'Event Settings', //module
+));
+
+?>
